9cddf6e04a4f1a2dbda330d7c066c0edd1ddd274
Arnaud Morin <arnaud.morin@gmail.com>
2017-03-08 13:43:03 +0100

    Install ssh and configure it

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,5 +6,23 @@
 
 class ssh {
 
-  # TODO
+  package { 'openssh-server':
+    ensure  => latest,
+  }
+
+  file { '/etc/ssh/sshd_config':
+    ensure  => present,
+    source  => "puppet:///modules/${module_name}/sshd_config",
+    notify  => Service['sshd'],  # this sets up the relationship
+    mode    => '0600',
+    owner   => 'root',
+    group   => 'root',
+    require => Package['openssh-server'],
+  }
+  
+  service { 'sshd':
+    ensure  => 'running',
+    enable  => true,
+    require => Package['openssh-server'],
+  }
 }
